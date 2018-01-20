bbcb3be1d952f7d147b07ae50efdd9efc0ec961b
Jacqueline Askins <askinsj@sas.upenn.edu>
2018-01-20 13:17:13 -0500

    Ability to add and view tracked sites in Options

---
+++ options.js
@@ -0,0 +1,73 @@
+function millisecondsToHours(duration) {
+  return parseFloat(duration) / 3600000;
+}
+
+function hoursToMilliseconds(duration) {
+  return parseFloat(duration) * 3600000;
+}
+
+function addSite(e) {
+  e.preventDefault();
+  var errorMessages = document.getElementById('error_messages');
+  errorMessages.innerHtml = '';
+
+  var addSiteButton = document.getElementById('add_site');
+  addSiteButton.disabled = true;
+
+  var siteNameInput = document.getElementById('site_name');
+  var siteName = siteNameInput.value;
+  var siteURLInput = document.getElementById('site_url');
+  var siteURL = siteURLInput.value;
+  var timeLimitInput = document.getElementById('time_limit');
+  var timeLimit = timeLimitInput.value;
+
+  if (parseFloat(timeLimit) > 24) {
+    errorMessages.innerHTML += 'Time limit must be less than 24 hours.';
+    addSiteButton.disabled = false;
+    return;
+  }
+
+  if (siteName !== '' && siteURL !== '' && timeLimit !== '') {
+    chrome.storage.sync.get("sites", function(data) {
+      var sites = data.sites || {};
+
+      if (siteURL in sites) {
+        errorMessages.innerHTML += 'You already have a time limit set for this URL.';
+        return;
+      }
+      sites[siteURL] = { 'name': siteName, 'timeLimit': hoursToMilliseconds(timeLimit) };
+      chrome.storage.sync.set({ 'sites': sites }, function() {
+        if (chrome.runtime.lastError) {
+          errorMessages.innerHTML += 'There was an error adding your time limit. Please try again.';
+          return;
+        }
+        document.getElementById('success').innerHTML = 'You successfully added a time limit for ' + siteURL;
+        siteNameInput.value = '';
+        siteURLInput.value = '';
+        timeLimitInput.value = '';
+        updateTrackedSites();
+      });
+    });
+  }
+
+  addSiteButton.disabled = false;
+}
+
+function updateTrackedSites() {
+  var trackedSites = document.getElementById('tracked_sites');
+  trackedSites.innerHTML = '';
+  chrome.storage.sync.get('sites', function(data) {
+    var sites = data.sites || {};
+    console.log(sites);
+    Object.keys(sites).forEach(function(site) {
+      trackedSites.innerHTML += (
+        "<tr><td>" + sites[site]['name'] + "</td><td>" + site + "</td><td>" + millisecondsToHours(sites[site]['timeLimit']) + " hours</td></tr>"
+      )
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById('add_site_form').addEventListener('submit', function(event) { addSite(event) });
+  updateTrackedSites();
+});
